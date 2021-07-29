be18253f5cb7f6577f65d281368aed47812a893c
virruru <87971673+virruru@users.noreply.github.com>
2021-07-29 14:40:22 +0530

    massofearth

---
+++ massofearth/src/massofearth/Mass.java
@@ -0,0 +1,12 @@
+package massofearth;
+
+public class Mass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double s = 5.972*Math.pow(10,24);
+		System.out.println(s);
+
+	}
+
+}
